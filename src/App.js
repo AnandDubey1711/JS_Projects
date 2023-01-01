4df006ab59b242831ed8eb10387d959a08f9ae97
Coder_Yeager <110890327+AnandDubey1711@users.noreply.github.com>
2023-01-01 13:15:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,47 +9,47 @@ const App = () => {
     </div>'
       <Cards link="https://images.pexels.com/photos/3400795/pexels-photo-3400795.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
       title="project1" project="Tic Tac Toe" description="This is a gaming JavaScript Project which allows the user to play Tic Tac Toe against an AI with its own set of Algorithms"
-      project_link="https://tic-tac-toe-akd1711.netlify.app"
+      project_link="https://github.com/AnandDubey1711/JavaScript-Projects"
       demo="https://tic-tac-toe-akd1711.netlify.app"   />
 
       <Cards link="https://images.pexels.com/photos/6373305/pexels-photo-6373305.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
       title="project2" project="HomePage" description="This is a JavaScript Project which consits of a portfolio webpage for any individual which allows the recuiter to get an overview"
-      project_link="https://homepage-akd.netlify.app"
+      project_link="https://github.com/AnandDubey1711/JavaScript-Projects"
       demo="https://homepage-akd.netlify.app"   />
 
       <Cards link="https://images.pexels.com/photos/2480854/pexels-photo-2480854.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
       title="project3" project="Gallery" description="This is a JavaScript Project which shows an gallery to display multiple images for a certain product or e-commerce website"
-      project_link="https://gallery-akd.netlify.app"
+      project_link="https://github.com/AnandDubey1711/JavaScript-Projects"
       demo="https://gallery-akd.netlify.app"   />
 
       <Cards link="https://images.pexels.com/photos/3194469/pexels-photo-3194469.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
       title="project4" project="Music Player" description="This is a music player which is developed using JavaScript and there are multiple control buttons and songs to choose from for a user"
-      project_link="https://music-player-app-akd1711.netlify.app"
+      project_link="https://github.com/AnandDubey1711/JavaScript-Projects"
       demo="https://music-player-app-akd1711.netlify.app"   />
 
       <Cards link="https://images.pexels.com/photos/1329295/pexels-photo-1329295.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
       title="project5" project="Counter" description="This is a JavaScript Project which allows the user to count the numbers on click in both positive and negative direction"
-      project_link="https://counter-app-akd.netlify.app"
+      project_link="https://github.com/AnandDubey1711/JavaScript-Projects"
       demo="https://counter-app-akd.netlify.app"   />
 
       <Cards link="https://images.pexels.com/photos/40799/paper-colorful-color-loose-40799.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
       title="project6" project="Color Generator" description="This is a JavaScript Project which generates random combination of colors ranging from simple color values and hexadecimal values"
-      project_link="https://chipper-treacle-60faf0.netlify.app"
+      project_link="https://github.com/AnandDubey1711/JavaScript-Projects"
       demo="https://chipper-treacle-60faf0.netlify.app"   />
 
       <Cards link="https://soliloquywp.com/wp-content/uploads/2016/08/How-to-Create-an-Image-Slider-in-WordPress-2.png"
       title="project7" project="Image Slider" description="This is a JavaScript Project which allows the user to insert multiple images which changes from one image to another"
-      project_link="https://imageslider-akd1711.netlify.app"
+      project_link="https://github.com/AnandDubey1711/JavaScript-Projects"
       demo="https://imageslider-akd1711.netlify.app"   />
 
       <Cards link="https://www.bgs.org.uk/sites/default/files/styles/nugget-normal/public/content/paragraphs/text/images/2019-09-16/tree-838667_1280.jpg?itok=NMphtSyc&timestamp=1568645426"
       title="project8" project="Login Page" description="This project is developed using JavaScript and consits of an interactive login page which can be used for websites"
-      project_link="https://login-page-akd1711.netlify.app"
+      project_link="https://github.com/AnandDubey1711/JavaScript-Projects"
       demo="https://login-page-akd1711.netlify.app"   />
 
       <Cards link="https://www.irpcommerce.com/IRPStrategyCenter/Interface/DisplayImages/55.jpg"
       title="project9" project="Reviews Page" description="This is a gaming JavaScript Project which consits of multiple reviews that can be used by a business to show promote themselves"
-      project_link="https://review-akd1711.netlify.app"
+      project_link="https://github.com/AnandDubey1711/JavaScript-Projects"
       demo="https://review-akd1711.netlify.app"   />
     </div>
   )
